92de70ee7de0cb5dd21dc0c589e4f112811fa11d
Fatıma Ekşioğlu <fatunch@gmail.com>
2023-12-11 11:22:05 +0300

    deneme commit

--- pair8/src/main/java/com/tobeto/pair8/Pair8Application.java
+++ pair8/src/main/java/com/tobeto/pair8/Pair8Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Pair8Application {
 
+	// Https deneme
 	public static void main(String[] args) {
 		SpringApplication.run(Pair8Application.class, args);
 	}
