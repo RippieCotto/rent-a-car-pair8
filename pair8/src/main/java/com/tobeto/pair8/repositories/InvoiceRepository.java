a06bb6736f7b9502faefdc5ba4e7cc21a5e1cfb5
PC <emrcnn774@gmail.com>
2023-12-14 12:13:51 +0300

    update

--- pair8/src/main/java/com/tobeto/pair8/repositories/InvoiceRepository.java
+++ pair8/src/main/java/com/tobeto/pair8/repositories/InvoiceRepository.java
@@ -4,4 +4,5 @@ import com.tobeto.pair8.entities.Invoice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InvoiceRepository extends JpaRepository<Invoice, Integer> {
+
 }
